28f8ccbaab49aa159637fd0fec6ee640794c3fbe
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2024-01-05 15:11:12 +0100

    Implement messaging layer

---
+++ src/main/java/de/samples/javaee/richfaces/jms/sample/messaging/config/BlogPostEventsTopic.java
@@ -0,0 +1,14 @@
+package de.samples.javaee.richfaces.jms.sample.messaging.config;
+
+import jakarta.inject.Qualifier;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.TYPE, ElementType.PARAMETER})
+public @interface BlogPostEventsTopic {
+}
